70a5f4ba74491b7d5e5a50cf571b011ba39c09c3
pranitpotsure <pranitpotsure55@gmail.com>
2026-02-13 21:58:08 +0530

    all new code

--- terraform-infra/SecurityGroup.tf
+++ terraform-infra/SecurityGroup.tf
@@ -49,6 +49,12 @@ resource "aws_security_group" "internal_alb_sg" {
     protocol        = "tcp"
     security_groups = [aws_security_group.web_sg.id] # traffic allowed form web SG 
   }
+   egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # This for App tier  
